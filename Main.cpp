3599e266772f9dbb1cfe320800ddb15c87b88f88
Уханов Антон <tosha.uhanoff@yandex.ru>
2017-03-17 18:16:16 +0300

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,73 @@
+
+#include "stdafx.h"
+#include "TInteger.h"
+#include "iostream"
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	TInteger a(0);
+	std::cout << a << std::endl; //0
+	TInteger b(4);
+	std::cout << b << std::endl; //4
+	a = b;
+	std::cout << a << std::endl; //4
+
+	a += b;
+	std::cout << a << std::endl; //8
+	a = a + 2;
+	std::cout << a << std::endl; //10
+
+
+	a -= b;
+	std::cout << a << std::endl; //6
+	a = a- -4;
+	std::cout << a << std::endl; // 10
+
+
+	a = a * 5;
+	std::cout << a << std::endl; // 50
+	a *= b;
+	std::cout << a << std::endl; // 200
+
+	a = a / b;
+	std::cout << a << std::endl; //50
+	a /= 5;
+	std::cout << a << std::endl; //10
+	a -= 6;
+	if (a == b)
+	{
+		std::cout << "a == b" << std::endl;
+	}
+	a += 6;
+	if (a!= b)
+	{
+		std::cout << "a != b" << std::endl;
+	}
+
+	if (a > b && a > 4)
+	{
+		std::cout << "a > b && a > 4 (b = 4, a = 10)" << std::endl;
+	}
+	a = 3;
+	if (a < b && a < 4)
+	{
+		std::cout << "a < b && a < 4 (a = 3)" << std::endl;
+	}
+	a = 5;
+	if (a <= b && a <= 4)
+	{
+		std::cout << "a <= b(4)" << std::endl;
+	}
+	if (a >= b && a >= 4)
+	{
+		std::cout << "a >= b(4) " << std::endl;
+	}
+	a = 10;
+	std::cout <<"a % b =  "<< a % b << std::endl;
+	std::cout << "a % 6 =  " << a % 6 << std::endl;
+	a++;
+	++a;
+	std::cout << a << std::endl;
+	system("pause");
+	return 0;
+}
